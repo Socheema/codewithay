4acbd33410618b21622546d727879041adb0fb24
Azubuike Chima <chimafrancis94@gmail.com>
2022-06-26 00:43:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// var image = document.getElementById('image');
+// var menu = document.getElementById('menu');
+// var body = document.getElementsByTagName('body');
+// console.log(body)
+
+// menu.addEventListener('click', runEvent)
+// function runEvent(){
+//     menu.style.visibility = "visible";
+//     body.style.backgroundColor = "red";
+
+// }
